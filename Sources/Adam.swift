feea3180ddf7fb72c2bbffee6ab5f555c5a312f6
MauriceElliott <mauricemelliott@gmail.com>
2025-09-16 22:36:53 +0100

    playing about while waiting for emerge to finish running

--- Sources/Adam.swift
+++ Sources/Adam.swift
@@ -4,20 +4,47 @@
 // As little as possible should be defined here.
 // This is only for the truely important.
 
-@_cdecl("kernel_main")
-public func kernel_main() -> Never {
-    let vga = UnsafeMutablePointer<UInt16>(bitPattern: 0xB8000)!
-    let attr = UInt16(0x0F) << 8
+public struct VGA {
+    let buffer: UnsafeMutablePointer<UInt16>
+    var attribute: UInt16 = 0x0F << 8
+}
 
-    let bootMessage: StaticString = "Hello World!"
-    bootMessage.withUTF8Buffer { buf in
+private func InitialiseVga() -> VGA {
+    return VGA(buffer: UnsafeMutablePointer<UInt16>(bitPattern: 0xB8000)!)
+}
+
+public func KernelPanic() {
+    print("Crashing!!!")
+}
+
+public func WriteToVga(_ input: StaticString,_ vga: VGA) {
+    input.withUTF8Buffer({ buf in
         var i = 0
         while i < buf.count {
-            vga[i] = attr | UInt16(buf[i])
+            vga.buffer[i] = vga.attribute | UInt16(buf[i])
             i += 1
         }
+    })
+}
+
+private func BootMessage(vga: VGA) -> Bool {
+    WriteToVga("=================================", vga)
+    WriteToVga("=== LAUNCH SEQUENCE INITIATED ===", vga)
+    WriteToVga("=================================", vga)
+    return false;
+}
+
+@_cdecl("kernel_main")
+public func kernel_main() -> Never {
+    let vga = InitialiseVga()
+  
+    if BootMessage(vga: vga) {
+        while true {
+            //halt
+        }
+    } else {
+        KernelPanic()
     }
-    
     while true {
         //halt
     }
