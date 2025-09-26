5b08afeb680e9088bd1fb1c46f5f79eaafd23057
MauriceElliott <mauricemelliott@gmail.com>
2025-09-26 22:33:39 +0100

    Middle of implementing boot message and have run into a hardware limitation with the uart serial port. or more specifically, the buffer that interacts with it.

--- Sources/Core/Adam.swift
+++ Sources/Core/Adam.swift
@@ -3,25 +3,64 @@
 // This is the Terminal Dogma of MAGIos.
 // As little as possible should be defined here.
 
-@_cdecl("kernel_main")
-public func kernel_main() -> Never {
+public func printLine(_ string: String){
     let uart_out_register: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer(bitPattern: 0x10000000)!
-    let uart_line_status_register: UnsafePointer<UInt8> = UnsafePointer(bitPattern: 0x10000005)!
-    
-    func putchar(_ character: UInt8) {
         // Wait until UART is ready (bit 5 of LSR = Transmission holding register is empty)
-        while (uart_line_status_register.pointee & 0x20) == 0 {
-            // Output is not ready for next character.
-        }
+    let uart_line_status_register: UnsafePointer<UInt8> = UnsafePointer(bitPattern: 0x10000005)!
+
+     func putChar(_ character: UInt8) {
+        while (uart_line_status_register.pointee & 0x20) == 0 { /*wait for uart to be ready for next character*/ }
+        for _ in 0..<100 { /* small delay */ }
         uart_out_register.pointee = character
     }
-    
 
-    let msg = "Hello World from inside MAGIos!\n"
+    let output = "\(string)"
 
-    for byte in msg.utf8 {
-        putchar(byte)
+    for byte in output.utf8 {
+        putChar(byte)
     }
+}
 
+// SO essentially, the issue is the register that passes the bytes to uart is overflowing, its only 16 bytes in size
+// And so we need to chunk the data and slowly send it chunk by chunk to the the uart_out_register by essentially
+// calulating the size of the array and then sending it bit by bit.
+// I am exhausted after this week of sickness though so will aim to pick up tomorrow. Hopefully this can help
+// remind me of what I was doing.
+
+
+public func bootMessage() {
+    printLine("============================================================")
+//     printLine("================ Entering Central Dogma ====================")
+//     printLine("============================================================")
+
+//     printLine("\n")
+//     printLine("BAL Boot Successful...initiating MEL boot sequence...")
+//     printLine("")
+//     printLine("MEL Boot Successful...initiating CAS boot sequence...")
+//     printLine("")
+//     printLine("CAS Boot Successful....")
+//     printLine("All MAGI Have come online....")
+//     printLine("MAGI Sync Initiated....")
+//     printLine(".")
+//     printLine("..")
+//     printLine("...")
+//     printLine(".....")
+//     printLine("...........")
+//     printLine("............")
+//     printLine("............")
+//     printLine("..............................")
+//     printLine("...........................................................")
+//     printLine("\n")
+//     printLine("Syncronisation Complete, all systems Nominal")
+//     printLine("\n\n Good Morning Professor")
+}
+
+@_cdecl("kernel_main")
+public func kernel_main() -> Never {
+    // bootMessage()
+    // 
+    printLine("Hello Worldsss")
+    printLine("............")
+    
     while true {}
 }
