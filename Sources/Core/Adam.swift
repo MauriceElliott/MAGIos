9375779d7fd96491d7f8056b97eec79453c76b25
MauriceElliott <mauricemelliott@gmail.com>
2025-09-28 00:45:28 +0100

    Looks like I might need to implement some of the standard libary to make this work in a dynamic way which I think isn't sucha bad move, so we'll be working on that next time

--- Sources/Core/Adam.swift
+++ Sources/Core/Adam.swift
@@ -3,45 +3,34 @@
 // This is the Terminal Dogma of MAGIos.
 // As little as possible should be defined here.
 
-public func printLine(_ string: String){
-    func putChar(_ character: UInt8,_ uart_out: UnsafeMutablePointer<UInt8>,_ uart_line_stat: UnsafePointer<UInt8>) {
-        while (uart_line_stat.pointee & 0x20) == 0 { /*wait for uart to be ready for next character*/ }
-        for _ in 0..<100 { /* small delay */ }
-        uart_out.pointee = character
-    }
+public func printChunkToUart(_ string: String){
+    let uart_out_register: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer(bitPattern: 0x10000000)!
+    let uart_line_status_register: UnsafePointer<UInt8> = UnsafePointer(bitPattern: 0x10000005)!
 
-    let output = "\(string)\n"
-    var uart_out_register: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer(bitPattern: 0x10000000)!
-    var uart_line_status_register: UnsafePointer<UInt8> = UnsafePointer(bitPattern: 0x10000005)!
-    var count = 0
-    for byte in output.utf8 {
-        if count % 15 == 0 {
-            uart_out_register = UnsafeMutablePointer(bitPattern: 0x10000000)!
-            uart_line_status_register = UnsafePointer(bitPattern: 0x10000005)!
-        }
-        putChar(byte, uart_out_register, uart_line_status_register)
-        count += 1
+    func putChar(_ character: UInt8) {
+        while (uart_line_status_register.pointee & 0x20) == 0 { /*wait for uart to be ready for next character*/ }
+        for _ in 0..<100 { /* small delay */ }
+        uart_out_register.pointee = character
+    }
+    for byte in string.utf8 {
+        putChar(byte)
     }
 }
 
-// public func printLine2(_ string: String){
-//     let uart_out_register: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer(bitPattern: 0x10000000)!
-//         // Wait until UART is ready (bit 5 of LSR = Transmission holding register is empty)
-//     let uart_line_status_register: UnsafePointer<UInt8> = UnsafePointer(bitPattern: 0x10000005)!
-
-//      func putChar(_ character: UInt8) {
-//         while (uart_line_status_register.pointee & 0x20) == 0 { /*wait for uart to be ready for next character*/ }
-//         for _ in 0..<100 { /* small delay */ }
-//         uart_out_register.pointee = character
-//     }
-
-//     let output = "\(string)"
-
-//     for byte in output.utf8 {
-//         putChar(byte)
-//     }
-// }
-
+public func printLine(_ string: String) {
+    var chunks = Array<String>()
+    var chunk = ""
+    for (index, c) in string.enumerated() {
+        if index % 14 == 0 {
+            chunks.append(chunk)
+        } else {
+            chunk += "\(c)"
+        }
+    }
+    for chunk in chunks {
+        printChunkToUart(chunk)
+    }
+}
 
 // SO essentially, the issue is the register that passes the bytes to uart is overflowing, its only 16 bytes in size
 // And so we need to chunk the data and slowly send it chunk by chunk to the the uart_out_register by essentially
