e2ccfcbba5eab277d25667d1b51291d56e0bbab9
MauriceElliott <mauricemelliott@gmail.com>
2025-09-29 22:26:32 +0100

    Still working through, see comments in Adam.swift for details

--- Sources/Core/Adam.swift
+++ Sources/Core/Adam.swift
@@ -18,25 +18,35 @@ public func printChunkToUart(_ string: String){
 }
 
 public func printLine(_ string: String) {
-    var chunks = Array<String>()
-    var chunk = ""
-    for (index, c) in string.enumerated() {
-        if index % 14 == 0 {
-            chunks.append(chunk)
-        } else {
-            chunk += "\(c)"
-        }
-    }
-    for chunk in chunks {
-        printChunkToUart(chunk)
+    let bytes = Array(string.utf8)
+    let chunkSize = 14
+    printChunkToUart(string)
+    for i in stride(from: 0, to: bytes.count, by: chunkSize) {
+        let endIndex = min(i + chunkSize, bytes.count)
+        let chunk = bytes[i..<endIndex]
+        let chunkString = String(validating: chunk, as: UTF8.self) ?? ""
+        printChunkToUart(chunkString)
     }
 }
 
+// PART 1
 // SO essentially, the issue is the register that passes the bytes to uart is overflowing, its only 16 bytes in size
 // And so we need to chunk the data and slowly send it chunk by chunk to the the uart_out_register by essentially
 // calulating the size of the array and then sending it bit by bit.
 // I am exhausted after this week of sickness though so will aim to pick up tomorrow. Hopefully this can help
 // remind me of what I was doing.
+// PART 2
+// So now we've run into an issue where seemingly we are calling on things from the standard libary, which is causing issues.
+// We need to either build the OS binary so that the Standard Libary is built in. i.e. Freestanding.
+// Or we need to look at implementing the things ourselves. Or just find a more straight forward way to implement them.
+// NOTES FOR NEXT TIME
+// Array seems to be causing issues too, For some reason when its called the output to uart is lost, where as when its removed
+// It works fine. I think we're going in the right direction with the rest, but at the same time I'm wondering if maybe
+// I should just properly dumb it down, convert every string to UInt8 characters, maybe use a different string type
+// like static.
+// This is new territory for me so making it through is important, but so is learning here and I am having fun trying
+// to work this out. I guess I just need to avoid putting pressure on completing this or getting a result.
+// Its a personal project, a hobby, its only for me to see progress.
 
 
 public func bootMessage() {
@@ -52,15 +62,6 @@ public func bootMessage() {
     printLine("CAS Boot Successful....")
     printLine("All MAGI Have come online....")
     printLine("MAGI Sync Initiated....")
-    printLine(".")
-    printLine("..")
-    printLine("...")
-    printLine(".....")
-    printLine("...........")
-    printLine("............")
-    printLine("............")
-    printLine("..............................")
-    printLine("...........................................................")
     printLine("\n")
     printLine("Syncronisation Complete, all systems Nominal")
     printLine("\n\n Good Morning Professor")
@@ -70,7 +71,8 @@ public func bootMessage() {
 public func kernel_main() -> Never {
     // bootMessage()
     // printLine2("hello2")
+    // printChunkToUart("Hello")
     printLine("Hello World!!!")// Max limit for 1 chunk
-    printLine("Hello World!!!!")//Overflow into second chunk
+    // printLine("Hello World!!!!")//Overflow into second chunk
     while true {}
 }
