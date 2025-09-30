e8fe6ceb53fa0afb6a5efe5052fa0183adb8dd61
MauriceElliott <mauricemelliott@gmail.com>
2025-09-30 21:40:34 +0100

    Fixed! Chunking when printing to uart serial port now works!!!

--- Sources/Core/Adam.swift
+++ Sources/Core/Adam.swift
@@ -17,7 +17,7 @@ public func printChunkToUart(_ bytes: UnsafeBufferPointer<UInt8>){
     }
 }
 
-public func printLine(_ string: StaticString) {
+public func print(_ string: StaticString) {
     let totalBytes = string.utf8CodeUnitCount
     let bytesPtr = string.utf8Start
     let chunkSize = 14
@@ -37,42 +37,22 @@ public func printLine(_ string: StaticString) {
     }
 }
 
-// PART 1
-// SO essentially, the issue is the register that passes the bytes to uart is overflowing, its only 16 bytes in size
-// And so we need to chunk the data and slowly send it chunk by chunk to the the uart_out_register by essentially
-// calulating the size of the array and then sending it bit by bit.
-// I am exhausted after this week of sickness though so will aim to pick up tomorrow. Hopefully this can help
-// remind me of what I was doing.
-// PART 2
-// So now we've run into an issue where seemingly we are calling on things from the standard libary, which is causing issues.
-// We need to either build the OS binary so that the Standard Libary is built in. i.e. Freestanding.
-// Or we need to look at implementing the things ourselves. Or just find a more straight forward way to implement them.
-// NOTES FOR NEXT TIME
-// Array seems to be causing issues too, For some reason when its called the output to uart is lost, where as when its removed
-// It works fine. I think we're going in the right direction with the rest, but at the same time I'm wondering if maybe
-// I should just properly dumb it down, convert every string to UInt8 characters, maybe use a different string type
-// like static.
-// This is new territory for me so making it through is important, but so is learning here and I am having fun trying
-// to work this out. I guess I just need to avoid putting pressure on completing this or getting a result.
-// Its a personal project, a hobby, its only for me to see progress.
-
-
 public func bootMessage() {
-    printLine("============================================================\n")
-    printLine("================ Entering Central Dogma ====================\n")
-    printLine("============================================================\n")
+    print("============================================================\n")
+    print("================ Entering Central Dogma ====================\n")
+    print("============================================================\n")
 
-    printLine("\n")
-    printLine("BAL Boot Successful...initiating MEL boot sequence...\n")
-    printLine("\n")
-    printLine("MEL Boot Successful...initiating CAS boot sequence...\n")
-    printLine("\n")
-    printLine("CAS Boot Successful....\n")
-    printLine("All MAGI Have come online....\n")
-    printLine("MAGI Sync Initiated....\n")
-    printLine("\n")
-    printLine("Syncronisation Complete, all systems Nominal\n")
-    printLine("\n\n Good Morning Professor\n")
+    print("\n")
+    print("BAL Boot Successful...initiating MEL boot sequence...\n")
+    print("\n")
+    print("MEL Boot Successful...initiating CAS boot sequence...\n")
+    print("\n")
+    print("CAS Boot Successful....\n")
+    print("All MAGI Have come online....\n")
+    print("MAGI Sync Initiated....\n")
+    print("\n")
+    print("Syncronisation Complete, all systems Nominal\n")
+    print("\n\n Good Morning Professor\n")
 }
 
 @_cdecl("kernel_main")
